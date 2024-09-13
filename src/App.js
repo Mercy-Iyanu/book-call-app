24e27aba9ca73b5851fa180b2299b7080c32ffd7
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-09-13 15:49:14 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 
 export default function App () {
     const [fetchedData, setFetchedData] = useState([]);
@@ -28,17 +30,27 @@ export default function App () {
         )
     }
 
-    
+
     return (
         <div>
             <h1>Rendering item lists</h1>
-            <ul>
+            <Swiper
+                // spaceBetween={50}  // Space between each slide
+                // slidesPerView={1}   // Number of slides visible at a time
+                // navigation         // Add navigation buttons
+                // pagination={{ clickable: true }} 
+            >
                 {
                     fetchedData.map((item) => (
-                        <li key={item.id}>{item.title}</li>
+                        <SwiperSlide key={item.id}>
+                            <div style={{ padding: '20px', border: '1px solid #ccc' }}>
+                                <h1>{item.title}</h1>
+                                <h4>{item.body}</h4>
+                            </div>
+                        </SwiperSlide>
                     ))
                 }
-            </ul>
+            </Swiper>
         </div>
     )
 }
